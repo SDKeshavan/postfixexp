28e15e88fecb52362c32cb16fa063cd55376bd2c
Devesh Keshavan S <87265398+SDKeshavan@users.noreply.github.com>
2023-07-07 21:59:09 +0530

    Update postfixevalnew.js
    Direct postfix exp with spaces can be given

--- postfixevalnew.js
+++ postfixevalnew.js
@@ -8,54 +8,19 @@ var te=0;
 
 function convert(){
     var oriExp = document.forms["inputForm"]["expression"].value;    
-    var oriExpArr=[]
     var temp="";
-    for(var i=0;i<oriExp.length;i++){
-        if(!operator.includes(oriExp[i])){
+
+    for(i=0;i<oriExp.length;i++){
+        if(oriExp[i]!=" "){
             temp+=oriExp[i];
         }else{
-            oriExpArr.push(temp);
-            oriExpArr.push(oriExp[i]);
+            postFixExp.push(temp);
             temp="";
         }
     }
 
     if(temp!=""){
-        oriExpArr.push(temp);
-    }
-
-    var operatorStack=[];
-    var top=-1;
-
-    for(var j=0;j<oriExpArr.length;j++){
-        if(operator.includes(oriExpArr[j])){
-            if(top==-1){
-                operatorStack.push(oriExpArr[j]);
-                top+=1;
-            }else{
-                if(operator.indexOf(operatorStack[top])<=operator.indexOf(oriExpArr[j])){
-                    operatorStack.push(oriExpArr[j]);
-                    top+=1;
-                }else{
-                    while(operator.indexOf(operatorStack[top])>operator.indexOf(oriExpArr[j])){
-                        postFixExp.push(operatorStack.pop());
-                        top-=1;
-                    }
-                    operatorStack.push(oriExpArr[j]);
-                    top+=1;
-                }
-            }
-        }else{
-            postFixExp.push(parseFloat(oriExpArr[j]));
-        }
-    }
-
-
-    if(top>=0){
-        while(top>=0){
-            postFixExp.push(operatorStack.pop());
-            top-=1;
-        }
+        postFixExp.push(temp);
     }
 
     for(var m=0;m<postFixExp.length;m++){
@@ -66,9 +31,7 @@ function convert(){
             }
             ,500*m,m);
     }
-
 }
-
 
 var k=0;
 
@@ -106,14 +69,14 @@ function eval(){
                 postFixExpeval[te-1].style.border="1px solid #000";
             },700,te);
             
-            var num2=operandArr.pop();
+            var num2=parseInt(operandArr.pop());
 
             setTimeout((te)=>{
                 postFixExpeval[te].innerHTML="";
             },700,te);
             
             te-=1;
-            var num1=operandArr.pop();
+            var num1=parseInt(operandArr.pop());
             setTimeout((te)=>{
                 postFixExpeval[te].innerHTML="";
             },700,te);
